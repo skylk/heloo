83e1afeed8b6637cfbbe5f6614be9d3c47af5a34
skylk <skylk@centrum.cz>
2017-08-17 23:51:12 +0200

    a

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -49,73 +49,8 @@ var app = {
 };
 
 
-//First step check parameters mismatch and checking network connection if available call    download function
 
-function DownloadFile(URL, Folder_Name, File_Name) {
-//Parameters mismatch check
-if (URL == null && Folder_Name == null && File_Name == null) {
-    return;
-}
-else {
-    //checking Internet connection availablity
-    var networkState = navigator.connection.type;
-    if (networkState == Connection.NONE) {
-        return;
-    } else {
-        download(URL, Folder_Name, File_Name); //If available download function call
-    }
-  }
-}
-
-
-function download(URL, Folder_Name, File_Name) {
-//step to request a file system 
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
-
-function fileSystemSuccess(fileSystem) {
-    var download_link = encodeURI(URL);
-    ext = download_link.substr(download_link.lastIndexOf('.') + 1); //Get extension of URL
-
-    var directoryEntry = fileSystem.root; // to get root path of directory
-    directoryEntry.getDirectory(Folder_Name, { create: true, exclusive: false }, onDirectorySuccess, onDirectoryFail); // creating folder in sdcard
-    var rootdir = fileSystem.root;
-    var fp = rootdir.toURL(); // Returns Fulpath of local directory
-
-    fp = fp + "/" + Folder_Name + "/" + File_Name + "." + ext; // fullpath and name of the file which we want to give
-    // download function call
-    filetransfer(download_link, fp);
-}
-
-function onDirectorySuccess(parent) {
-    // Directory created successfuly
-}
-
-function onDirectoryFail(error) {
-    //Error while creating directory
-    alert("Unable to create new directory: " + error.code);
-}
-
-  function fileSystemFail(evt) {
-    //Unable to access file system
-    alert(evt.target.error.code);
- }
-}
-
-function filetransfer(download_link, fp) {
-var fileTransfer = new FileTransfer();
-// File download function with URL and local path
-fileTransfer.download(download_link, fp,
-                    function (entry) {
-                        alert("download complete: " + entry.fullPath);
-                    },
-                 function (error) {
-                     //Download abort errors or download failed errors
-                     alert("download error source " + error.source);
-                     //alert("download error target " + error.target);
-                     //alert("upload error code" + error.code);
-                 }
-            );
-}
+// ----------------------------------------------------------------------------
 
 function download2(fileEntry, uri, readBinaryData) {
 
@@ -149,29 +84,75 @@ function download2(fileEntry, uri, readBinaryData) {
             //}
         }
     );
-}
+};
 
-function a() {
+
+function otevri(file) {
+      cordova.plugins.fileOpener2.open(
+        file,
+        'application/pdf', // make sure this is the same mimetype as your file
+        {
+          error : function(e) {
+            alert('Error status: ' + e.status + ' - Error message: ' + e.message);
+          },
+          success : function () {
+            console.log('file opened successfully');
+          }
+        }
+      );
+    };
+    
+    
+function otevri2(soubor) {
+cordova.plugin.pDialog.init({
+    theme : 'HOLO_DARK',
+    progressStyle : 'SPINNER',
+    cancelable : true,
+    title : 'Prosím čekejte',
+    message : 'Stahuji soubor ...' + soubor
+});
 
 var fileTransfer = new FileTransfer();
-var uri = encodeURI("http://pujcovaninaradi.cz/download/cenik.pdf");
-var fileURL = '/storage/emulated/0/Download/file.pdf';
 
+var uri = encodeURI("http://pujcovaninaradi.cz/download/" + soubor);
+var fileURL = cordova.file.externalRootDirectory + 'Download/files.pdf';
+
+statusDom = document.querySelector('#status');
+
+	fileTransfer.onprogress = function(progressEvent) {
+		if (progressEvent.lengthComputable) {
+			var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+			statusDom.innerHTML = perc + "% loaded..." + progressEvent.total;       
+		} else {
+			if(statusDom.innerHTML == "") {
+				statusDom.innerHTML = "Loading";
+			} else {
+				statusDom.innerHTML += ".";
+			}
+		}
+	};
+
+ 
 fileTransfer.download(
     uri,
     fileURL,
     function(entry) {
-        alert("download complete: " + entry.toURL());
-    },
-    function(error) {
-        alert("download error source " + error.source);
-        alert("download error target " + error.target);
-        alert("download error code" + error.code);
-    },
-    null,
-    {
-        
-        
-    }
+      cordova.plugin.pDialog.dismiss();
+      var file = entry.toURL();
+      cordova.plugins.fileOpener2.open(
+        file,
+        'application/pdf',
+        {
+          error : function(e) {
+            alert('Error status: ' + e.status + ' - Error message: ' + e.message);
+          },
+          success : function () {
+            console.log('file opened successfully');
+          }
+        }
+    );
+  }
 );
-}
+};    
+
+
